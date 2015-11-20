276218d0f65ed7a264e882ee367f77add240098f
dennisschmock <dennisschmock@10.50.130.68>
2015-11-20 09:10:47 +0100

    Denne udgave er god

---
+++ src/buildabeer/BuildABeer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildabeer;
+
+/**
+ *
+ * @author dennisschmock
+ */
+public class BuildABeer implements BuildABeerInterface{
+
+    @Override
+    public double alcholCalculating() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double calculateIBU() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double calculateMash() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
